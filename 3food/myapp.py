192cd742a989246aaf1a6b6c965bab8d1fa61444
Rémy Léone <remy.leone@gmail.com>
2015-08-03 17:46:35 +0200

    Clean up the code

--- 3food/myapp.py
+++ 3food/myapp.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env python
 
-import pdb
 import time
 import subprocess
-import redis
 from threading import Thread
+
+import redis
 import matplotlib.pyplot as plt
-from flask import Flask, request, render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 r = redis.Redis()
 key = "base1"
 
-def adding_value(sleep=10, delta=5):
+
+def update_picture(x, y):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x, y)
+    fig.savefig("static/graph.png")
+
+
+def update(sleep=10, delta=5):
     while True:
-	temp = float (subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"]).strip("temp='C\n"))
-	r.rpush(key, temp)
-	lenght = r.llen(key)
-	x = range (lenght-delta, lenght)
-	y = r.lrange(key,lenght - delta, lenght)
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111)
-	ax1.plot(x,y)
-	fig.savefig("static/graph.png")
-	time.sleep(sleep)
+        temp = float(subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"]).strip("temp='C\n"))
+        r.rpush(key, temp)
+        temp_list_length = r.llen(key)
+        update_picture(
+            x=range(temp_list_length - delta, temp_list_length),
+            y=r.lrange(key, temp_list_length - delta, temp_list_length),
+        )
+        time.sleep(sleep)
+
 
 @app.route("/", methods=['GET'])
 def main(delta=5):
-    lenght = r.llen(key)
-    d = r.lrange(key, lenght - delta, lenght)
-    return render_template("template.html", 
-		temperatures=d,
-		delta=delta,
-	)
+    temp_list_length = r.llen(key)
+    d = r.lrange(key, temp_list_length - delta, temp_list_length)
+    return render_template("template.html",
+                           temperatures=d,
+                           delta=delta,
+                           )
+
 
 if __name__ == "__main__":
-    t = Thread(target=adding_value)
+    t = Thread(target=update)
     t.start()
     app.debug = True
     app.run()
